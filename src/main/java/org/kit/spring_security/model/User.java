acfa7c1ba80c5a481ec9941af30c896d79b13ebc
ankit970606 <ankit970606@gmail.com>
2025-04-14 11:24:12 +0530

    Update User.java

--- src/main/java/org/kit/spring_security/model/User.java
+++ src/main/java/org/kit/spring_security/model/User.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Table(name="users")
 public class User {
     @Id
-    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    @GeneratedValue(strategy= GenerationType.------------)
     private int id;
     private String username;
     private String password;
